5547a1cec2046c035f9512ffdfa5e27e1208ce45
VLADok228336 <97050152+VLADok228336@users.noreply.github.com>
2023-06-10 12:49:31 +0300

    PHP

--- PHP/reg.php
+++ PHP/reg.php
@@ -1,14 +1,16 @@
+<?
 $hostname = "localhost";
 $username = "myusername";
 $password = "Abc13579";
-$dbName = "products";
-$userstable = "users";
+$dbName = "accdata";
+$userstable = "accdatas";
 MYSQL_CONNECT($hostname,$username,$password) OR DIE("Не могу создать соединение ");
 
 @mysql_select_db("$dbName") or die("Не могу выбрать базу данных ");
 
-$query = "INSERT INTO $userstable VALUES('$name','$email', '$preference')";
+$query = "INSERT INTO $userstable VALUES('$name','$email', '$password')";
 
 $result = MYSQL_QUERY($query);
 MYSQL_CLOSE();
 PRINT "Информация о вас занесена в базу данных.";
+?>
